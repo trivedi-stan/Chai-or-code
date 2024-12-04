1a419efc19e3baf1aa4bbac634e3b89c5a6df8fd
PIYUSH TRIVEDI <85507845+trivedi-stan@users.noreply.github.com>
2024-12-04 16:59:22 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 121311
+let accountEmail = "12@gmail.com"
+var accountPassword = "13224"
+accountCity = "Lucknow"
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
